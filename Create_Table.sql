4afe7775975ad08de314579fa7d2fd5c47442e63
tendoMbambisa <139225394+tendoMbambisa@users.noreply.github.com>
2025-02-24 14:27:26 +0200

    Add files via upload

---
+++ Create_Table.sql
@@ -0,0 +1,18 @@
+CREATE TABLE production.pokemon (
+ id INT IDENTITY (1,1) PRIMARY KEY,
+ name VARCHAR (255) NOT NULL,
+ phone VARCHAR (10),
+ email VARCHAR (255));
+
+ CREATE TABLE production.trainer(
+ id INT IDENTITY (1,1) PRIMARY KEY,
+ first_name VARCHAR(50) NOT NULL,
+ last_name VARCHAR (50) NOT NULL,
+ email VARCHAR (255) NOT NULL UNIQUE,
+ phone VARCHAR(25),
+ poke_id INT NOT NULL,
+ FOREIGN KEY (poke_id)
+   REFERENCES production.pokemon(id) ON DELETE CASCADE ON UPDATE CASCADE -- ON CASCADE MEANS THIS SHOULD DELETE THE OTHER RECORD TOO
+   );
+
+   
