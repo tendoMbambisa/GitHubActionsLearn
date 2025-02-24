a98486c935171098fa5a03de859309cf16c9455b
tendoMbambisa <139225394+tendoMbambisa@users.noreply.github.com>
2025-02-24 15:48:24 +0200

    Update Create_Table.sql

--- Create_Table.sql
+++ Create_Table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE production.trainer (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    phone VARCHAR(25),
-    poke_id INT NOT NULL,
-    FOREIGN KEY (poke_id)
-        REFERENCES production.pokemon (id)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+       id INT AUTO_INCREMENT PRIMARY KEY,
+       first_name VARCHAR(50) NOT NULL,
+       last_name VARCHAR(50) NOT NULL,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       phone VARCHAR(25),
+       poke_id INT NOT NULL,
+       FOREIGN KEY (poke_id)
+          REFERENCES production.pokemon (id)
+          ON DELETE CASCADE
+         ON UPDATE CASCADE
 );
