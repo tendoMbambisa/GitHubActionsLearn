ed926d4a53712b7894aeeb65dba9c1bf8a13fb6a
tendoMbambisa <139225394+tendoMbambisa@users.noreply.github.com>
2025-02-24 15:17:31 +0200

    Update Create_Table.sql

--- Create_Table.sql
+++ Create_Table.sql
@@ -1,19 +1,3 @@
--- sqlfluff: disable=PRS
-CREATE TABLE production.pokemon (
- id INT IDENTITY (1,1) PRIMARY KEY,
- name VARCHAR (255) NOT NULL,
- phone VARCHAR (10),
- email VARCHAR (255));
 
- CREATE TABLE production.trainer(
- id INT IDENTITY (1,1) PRIMARY KEY,
- first_name VARCHAR(50) NOT NULL,
- last_name VARCHAR (50) NOT NULL,
- email VARCHAR (255) NOT NULL UNIQUE,
- phone VARCHAR(25),
- poke_id INT NOT NULL,
- FOREIGN KEY (poke_id)
-   REFERENCES production.pokemon(id) ON DELETE CASCADE ON UPDATE CASCADE -- ON CASCADE MEANS THIS SHOULD DELETE THE OTHER RECORD TOO
-   );
--- sqlfluff: enable=PRS
-   
+CREATE TABLE production.pokemon (id INT IDENTITY (1,1) PRIMARY KEY,name VARCHAR (255) NOT NULL,phone VARCHAR (10),email VARCHAR (255));
+
