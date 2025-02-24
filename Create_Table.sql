b1345811cceef26db4c506d1a707e3af98f69229
tendoMbambisa <139225394+tendoMbambisa@users.noreply.github.com>
2025-02-24 15:25:00 +0200

    Update Create_Table.sql

--- Create_Table.sql
+++ Create_Table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE production.pokemon (id INT IDENTITY (1,1) PRIMARY KEY,name VARCHAR (255) NOT NULL,phone VARCHAR (10),email VARCHAR (255));
+CREATE TABLE production.pokemon(id INT IDENTITY (1,1) PRIMARY KEY,name VARCHAR (255) NOT NULL,phone VARCHAR (10),email VARCHAR (255));
