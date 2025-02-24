49b2cb0eeaeeb05b6225af6ef2bcdbccc9ae6cd2
tendoMbambisa <139225394+tendoMbambisa@users.noreply.github.com>
2025-02-24 15:32:18 +0200

    Update Create_Table.sql

--- Create_Table.sql
+++ Create_Table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `production`.`pokemon` (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name_First VARCHAR(255) NOT NULL,
     phone VARCHAR(10),
     email VARCHAR(255)
 );
