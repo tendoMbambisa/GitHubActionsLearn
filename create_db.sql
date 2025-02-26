f96509604634bb8316c6fcb2a3470dc2f18fe0be
tendoMbambisa <139225394+tendoMbambisa@users.noreply.github.com>
2025-02-26 10:53:07 +0200

    Create create_db.sql

---
+++ create_db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS my_local_db;
+USE my_local_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE
+);
